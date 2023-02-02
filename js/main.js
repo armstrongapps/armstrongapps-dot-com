ed0843c40db808b1091f572c3f774c11accabd3b
Derek Armstrong <derek@armstrongapps.com>
2023-02-01 23:49:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@ window.onload = function() {
 };
 
 function loadHeader() {
-    fetch('html-layout/header.html')
+    fetch('../html-layout/header.html')
     .then(response=> response.text())
     .then(text=> document.getElementById('header-element').innerHTML = text)
 }
 
 function loadFooter() {
-    fetch('html-layout/footer.html')
+    fetch('../html-layout/footer.html')
     .then(response=> response.text())
     .then(text=> document.getElementById('footer-element').innerHTML = text)
 }
 
 function loadContent() {
-    fetch('html-content/main.html')
+    fetch('../html-content/main.html')
     .then(response=> response.text())
     .then(text=> document.getElementById('content-element').innerHTML = text)
 }
